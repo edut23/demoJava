059bf9428c62d49074ad66a92838adf7c3a0388b
edut23 <edut23@gmail.com>
2024-03-19 18:58:56 -0300

    update

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -1,5 +1,15 @@
 package com.example.demo.controller;
 
-public class IndexController {
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+public class IndexController {
+    @GetMapping(value = "/", produces = "application/json")
+    public ResponseEntity init(@RequestParam(value = "nome", defaultValue = "none") String nome){
+        System.out.println("O param:" + nome);
+
+        return new ResponseEntity("Ola:" + nome, HttpStatus.OK);
+    }
 }
